685e47a13dc3b664476a3c94672ed4c6c4b37c27
RebecaGimenez <rebecagimenezavit@gmail.com>
2026-06-09 20:44:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function CalcularCompra() {
 
-    let sahumerios = parseInt(document.getElementById("sahumerios").value) || 0;
-    let aromatizantes = parseInt(document.getElementById("aromatizantes").value) || 0;
-    let velas = parseInt(document.getElementById("velas").value) || 0;
-    let defumadores = parseInt(document.getElementById("defumadores").value) || 0;
-    let difusores = parseInt(document.getElementById("difusores").value) || 0;
+    let sahumerios = Number(document.getElementById("sahumerios").value);
+    let aromatizantes = Number(document.getElementById("aromatizantes").value);
+    let velas = Number(document.getElementById("velas").value);
+    let defumadores = Number(document.getElementById("defumadores").value);
+    let difusores = Number(document.getElementById("difusores").value);
 
     let cantidadTotal =
         sahumerios +
@@ -34,21 +34,17 @@ function CalcularCompra() {
         velas;
 
     let neto =
-        (sahumerios * 1000) +
-        (aromatizantes * 2000) +
-        (velas * 3500) +
-        (defumadores * 2500) +
-        (difusores * 3000);
+        sahumerios * 1000 +
+        aromatizantes * 2000 +
+        velas * 3500 +
+        defumadores * 2500 +
+        difusores * 3000;
 
     let porcentaje = 0;
 
-    if (cantidadTotal > 20) {
-        porcentaje = 30;
-    } else if (cantidadTotal > 10) {
-        porcentaje = 15;
-    } else if (cantidadTotal > 5) {
-        porcentaje = 5;
-    }
+    if (cantidadTotal > 20) porcentaje = 30;
+    else if (cantidadTotal > 10) porcentaje = 15;
+    else if (cantidadTotal > 5) porcentaje = 5;
 
     let descuento = neto * porcentaje / 100;
     let total = neto - descuento;
