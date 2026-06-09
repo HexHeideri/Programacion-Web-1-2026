50cc2a09f7c55f9f4facaaaebe6b025112ba8dba
RebecaGimenez <rebecagimenezavit@gmail.com>
2026-06-09 20:45:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
             raizHtml.setAttribute("data-theme", "dark");
             iconoTema.textContent = "🌙";
         }
-    });
+    };
 
 function CalcularCompra() {
 
